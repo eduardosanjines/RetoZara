08ef62702a7277056fdfc48e951223a9901e4810
EduardoSanjines <eduardosanjines@gmail.com>
2018-07-10 12:08:26 +0200

    Extraer resutado Close #3

--- RetoZara/Program.cs
+++ RetoZara/Program.cs
@@ -52,6 +52,7 @@ namespace RetoZara
             //cierro fichero
             file.Close();
             Console.WriteLine(resultado_total);
+            //muestro por consola el resultado
             Console.ReadKey();
         }  
     }
